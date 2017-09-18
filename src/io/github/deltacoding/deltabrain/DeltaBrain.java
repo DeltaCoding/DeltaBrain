c0012369463622a8c7c7a790ba8f900563b4f953
DeltaCoding <deltadrizz@gmail.com>
2017-09-18 19:38:03 +0200

    Add HelloWorld file generation

--- src/io/github/deltacoding/deltabrain/DeltaBrain.java
+++ src/io/github/deltacoding/deltabrain/DeltaBrain.java
@@ -1,12 +1,28 @@
 package io.github.deltacoding.deltabrain;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import io.github.deltacoding.deltabrain.interpreter.BrainfuckInterpreter;
 
 public class DeltaBrain {
 
 	public static void main(String[] args) {
-		BrainfuckInterpreter bfi = new BrainfuckInterpreter(args[0]);
-		bfi.execute();
+		if(args[0].equals("genHW")) {
+			try {
+				File tmp = new File("HelloWorld.bf");
+				tmp.createNewFile();
+				String helloWorld = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
+				Files.write(Paths.get("HelloWorld.bf"), helloWorld.getBytes());
+			} catch (IOException e) {
+				System.err.println("[ERROR] Could not create/write to the file 'HelloWorld.bf'");
+			}
+		} else {
+			BrainfuckInterpreter bfi = new BrainfuckInterpreter(args[0]);
+			bfi.execute();
+		}
 	}
 	
 }
