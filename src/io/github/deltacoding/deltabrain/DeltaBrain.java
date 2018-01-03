ed3a752a1ab5a0575536ab88ed44eb5f5b45dd7c
DeltaCoding <deltadrizz@gmail.com>
2018-01-03 17:55:00 +0100

    Bugfix: Fixes #1

--- src/io/github/deltacoding/deltabrain/DeltaBrain.java
+++ src/io/github/deltacoding/deltabrain/DeltaBrain.java
@@ -10,18 +10,22 @@ import io.github.deltacoding.deltabrain.interpreter.BrainfuckInterpreter;
 public class DeltaBrain {
 
 	public static void main(String[] args) {
-		if(args[0].equals("genHW")) {
-			try {
-				File tmp = new File("HelloWorld.bf");
-				tmp.createNewFile();
-				String helloWorld = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
-				Files.write(Paths.get("HelloWorld.bf"), helloWorld.getBytes());
-			} catch (IOException e) {
-				System.err.println("[ERROR] Could not create/write to the file 'HelloWorld.bf'");
+		if(args.length >= 0) {
+			if(args[0].equals("genHW")) {
+				try {
+					File tmp = new File("HelloWorld.bf");
+					tmp.createNewFile();
+					String helloWorld = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
+					Files.write(Paths.get("HelloWorld.bf"), helloWorld.getBytes());
+				} catch (IOException e) {
+					System.err.println("[ERROR] Could not create/write to the file 'HelloWorld.bf'");
+				}
+			} else {
+				BrainfuckInterpreter bfi = new BrainfuckInterpreter(args[0]);
+				bfi.execute();
 			}
 		} else {
-			BrainfuckInterpreter bfi = new BrainfuckInterpreter(args[0]);
-			bfi.execute();
+			System.err.println("[ERROR] No file specified, please try again and add a file as an argument");
 		}
 	}
 	
